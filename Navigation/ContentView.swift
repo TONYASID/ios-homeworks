9f546e774617b72d686ba4e754aa2b7d25e00228
TonyAsid <kisloff_anton@mail.ru>
2022-06-08 10:02:48 +0500

    no message

---
+++ Navigation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Navigation
+//
+//  Created by  Admin on 07.06.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by  Admin on 07.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
